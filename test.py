0cb8be065250434e03281cb3c1541052fd83bfa5
Conistiree <30314751+Realheals@users.noreply.github.com>
2018-12-03 09:42:50 +1300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("Test")
+import random #D&D stat gen WIP
+attrlis=[]
+for y in range(0,6):
+    numlis=[]
+    for x in range(0,4):
+        numlis.append(random.randrange(1,7))
+    numlis.remove(min(numlis))
+    attrlis.append(sum(numlis))
+print(attrlis)
