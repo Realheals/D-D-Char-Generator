39370341f2c6e644c0e708fdaef48825b004b878
Conistiree <30314751+Realheals@users.noreply.github.com>
2018-12-06 20:10:58 +1300

    Update newtestup3.py

--- newtestup3.py
+++ newtestup3.py
@@ -1,9 +1,9 @@
 import random #D&D char gen WIP
 
 class DNDClass:
-    def __init__(self, n, b):
+    def __init__(self, n):
         self.name = n
-        self.background = b
+        self.background = ''
         self.statl = []
         self.skilllist=[]
         self.specialhealthmod=0
@@ -28,68 +28,36 @@ class DNDClass:
             stl[2]=stl[2]+1
             stl[3]=stl[3]+2
             self.specialhealthmod=+1 #REMEMBER TO ADD THIS TO HP SCORE
-        #if self.race == '
-        
+        #elif self.race == '
+
+    def addremovepart2(self, st1, st2, st3, st4, st5, st6):
+        stat2=self.statl
+        newstat=[0,0,0,0,0,0]
+        self.addremoveit(st1,stat2,newstat)
+        self.addremoveit(st2,stat2,newstat)
+        self.addremoveit(st3,stat2,newstat)
+        self.addremoveit(st4,stat2,newstat)
+        self.addremoveit(st5,stat2,newstat)
+        self.addremoveit(st6,stat2,newstat)
+        self.racemod(newstat)
+        self.statl=newstat
                     
 
     def calcstat2(self): #reference code [Str, Dex, Wis, Con, Int, Cha]
         stat2=self.statl
         newstat=[0,0,0,0,0,0]
-        do = self.addremoveit
         if self.chacla=='Rogue': #Stat Prio [Dex, Con, Cha, Str, Int, Wis]
-                self.addremoveit(1,stat2,newstat)
-                self.addremoveit(3,stat2,newstat)
-                self.addremoveit(5,stat2,newstat)
-                self.addremoveit(0,stat2,newstat)
-                self.addremoveit(4,stat2,newstat)
-                self.addremoveit(2,stat2,newstat)
-                self.racemod(newstat)
-                self.statl=newstat
+                self.addremovepart2(1,3,5,0,4,2)
         elif self.chacla=='Fighter' or self.chacla=='Barbarian' or self.chacla=='Paladin': #Stat Prio [Str, Con, Dex, Cha, Wis, Int]
-                self.addremoveit(0,stat2,newstat)
-                self.addremoveit(3,stat2,newstat)
-                self.addremoveit(1,stat2,newstat)
-                self.addremoveit(5,stat2,newstat)
-                self.addremoveit(2,stat2,newstat)
-                self.addremoveit(4,stat2,newstat)
-                self.racemod(newstat)
-                self.statl=newstat
+                self.addremovepart2(0,3,1,5,2,4)
         elif self.chacla=='Bard' or self.chacla=='Sorcerer' or self.chacla=='Warlock': #Stat Prio [Cha, Con, Dex, Int, Str, Wis]
-                self.addremoveit(5,stat2,newstat)
-                self.addremoveit(3,stat2,newstat)
-                self.addremoveit(1,stat2,newstat)
-                self.addremoveit(4,stat2,newstat)
-                self.addremoveit(0,stat2,newstat)
-                self.addremoveit(2,stat2,newstat)
-                self.racemod(newstat)
-                self.statl=newstat
+                self.addremovepart2(5,3,1,4,0,2)
         elif self.chacla=='Cleric' or self.chacla=='Druid': #Stat Prio [Wis, Con, Dex, Cha, Str, Int] #reference code [Str(0), Dex(1), Wis(2), Con(3), Int(4), Cha(5)]
-                self.addremoveit(2,stat2,newstat)
-                self.addremoveit(3,stat2,newstat)
-                self.addremoveit(1,stat2,newstat)
-                self.addremoveit(5,stat2,newstat)
-                self.addremoveit(0,stat2,newstat)
-                self.addremoveit(4,stat2,newstat)
-                self.racemod(newstat)
-                self.statl=newstat
+                self.addremovepart2(2,3,1,5,0,4)
         elif self.chacla=='Monk' or self.chacla=='Ranger': #Stat Prio [Dex, Wis, Con, Str, Int, Cha]
-                self.addremoveit(1,stat2,newstat)
-                self.addremoveit(2,stat2,newstat)
-                self.addremoveit(3,stat2,newstat)
-                self.addremoveit(0,stat2,newstat)
-                self.addremoveit(4,stat2,newstat)
-                self.addremoveit(5,stat2,newstat)
-                self.racemod(newstat)
-                self.statl=newstat
+                self.addremovepart2(1,2,3,0,4,5)
         elif self.chacla=='Wizard': #Stat Prio [Int, Con, Dex, Str, Wis, Cha]
-                self.addremoveit(4,stat2,newstat)
-                self.addremoveit(3,stat2,newstat)
-                self.addremoveit(1,stat2,newstat)
-                self.addremoveit(0,stat2,newstat)
-                self.addremoveit(2,stat2,newstat)
-                self.addremoveit(5,stat2,newstat)
-                self.racemod(newstat)
-                self.statl=newstat       
+                self.addremovepart2(4,3,1,0,2,5)
                 
 
     def printstat(self, l):
@@ -126,23 +94,78 @@ class DNDClass:
             attrlis.append(sum(numlis))
         self.statl=attrlis
 
-    def randoBG(self):
-        choicelist=['Outlander','Sage']
-        x= random.randrange(0,2)
-        self.background = choicelist[x]
 
+    def randoBG(self):
+        choicelist=['Outlander','Sage', 'Acolyte', 'Charlatan', 'Criminal', 'Entertainer', 'Folk Hero', 'Guild Artisan', 'Hermit', 'Noble', 'Sailor', 'Soldier', 'Urchin']
+        x= random.randrange(0,13)
+        self.background = choicelist[x]
+        if self.background == 'Sage':
+            self.skilllist.append('Arcana')
+            self.skilllist.append('History')
+        elif self.background == 'Outlander':
+            self.skilllist.append('Athletics')
+            self.skilllist.append('Survival')
+        elif self.background == 'Acolyte':
+            self.skilllist.append('Religion')
+            self.skilllist.append('Insight')
+        elif self.background == 'Charlatan':
+            self.skilllist.append('Deception')
+            self.skilllist.append('Sleight of Hand')
+        elif self.background == 'Criminal':
+            self.skilllist.append('Deception')
+            self.skilllist.append('Stealth')
+        elif self.background == 'Entertainer':
+            self.skilllist.append('Acrobatics')
+            self.skilllist.append('Performance')
+        elif self.background == 'Folk Hero':
+            self.skilllist.append('Animal Handling')
+            self.skilllist.append('Survival')
+        elif self.background == 'Guild Artisan':
+            self.skilllist.append('Insight')
+            self.skilllist.append('Persuasion')
+        elif self.background == 'Hermit':
+            self.skilllist.append('Medicine')
+            self.skilllist.append('Religion')
+        elif self.background == 'Noble':
+            self.skilllist.append('History')
+            self.skilllist.append('Persuasion')
+        elif self.background == 'Sailor':
+            self.skilllist.append('Athletics')
+            self.skilllist.append('Perception')
+        elif self.background == 'Soldier':
+            self.skilllist.append('Athletics')
+            self.skilllist.append('Intimidation')
+        elif self.background == 'Urchin':
+            self.skilllist.append('Sleight of Hand')
+            self.skilllist.append('Stealth')
     def randoSkill(self):
         RogueValidSkill=['Acrobatics', 'Athletics', 'Deception', 'Insight', 'Intimidation', 'Investigation', 'Perception', 'Persuasion', 'Sleight of Hand', 'Stealth']
         FighterValidSkill=['Acrobatics', 'Animal Handling', 'Athletics', 'History', 'Insight', 'Intimidation', 'Perception', 'Survival']
         BarbarianValidSkill=[]
+        self.mathrandoSkill(4, FighterValidSkill)
 
-    def mathrandoSkill():
-        Rge=10
-        Fig=8
-
+    def mathrandoSkill(self, amount, skillzee):
+        t=[]
+        print(self.skilllist[0])
+        if self.skilllist[0] in skillzee:
+            print("t")
+            t.append(skillzee.index(self.skilllist[0]))
+        if self.skilllist[1] in skillzee:
+            print("t")
+            t.append(skillzee.index(self.skilllist[1]))
+        x = 0
+        while x !=amount:
+            l = random.randrange(0, len(skillzee))
+            if l not in t:
+                self.skilllist.append(skillzee[l])
+                t.append(l)
+                x=x+1
+                
+            
     def pickClass(self):
-        classList=['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard']
-        x= random.randrange(0,12)
+        #classList=['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard']
+        classList=['Rogue', 'Fighter']
+        x= random.randrange(0,2)
         print(x)
         self.chacla=classList[x]
 
@@ -152,8 +175,8 @@ class DNDClass:
         print(x)
         self.race=raceList[x]
 
-c3 = DNDClass('Zeph', 'Outlander')
-c4 = DNDClass('Nowi', 'Sage')
+c3 = DNDClass('Zeph')
+c4 = DNDClass('Nowi')
 c3.randoBG()
 c4.randoBG()
 c3.pickClass()
@@ -165,15 +188,5 @@ c4.getstats()
 print(c4.statl)
 c4.calcstat2()
 print(c4.printstat(c4.statl))
-#c4.backg()
-
-#print(c3.printstat(c3.calcstat2()))
-#print(c1.introd())
-#c1.calcstat2()
-#print(c1.printstat(c1.calcstat2()))
-#print(" ")
-#print(c2.introd())
-#c2.calcstat2()
-#print(c2.printstat(c2.calcstat2()))
-#c1.backg()
-
+c4.randoSkill()
+print("Skilllist: " + ", ".join(c4.skilllist))
